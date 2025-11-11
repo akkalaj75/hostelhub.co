54bbf4309461d553de66f8f8a8637f356d6a4cfa
Jyothin manikanta reddy <158010808+akkalaj75@users.noreply.github.com>
2025-11-11 17:27:16 -0500

    Update fix-bugs.js

--- fix-bugs.js
+++ fix-bugs.js
@@ -1,13 +1,1 @@
-{
-  "indexes": [
-    {
-      "collectionGroup": "waiting",
-      "queryScope": "COLLECTION",
-      "fields": [
-        { "fieldPath": "hostel", "order": "ASCENDING" },
-        { "fieldPath": "interests", "arrayConfig": "CONTAINS" },
-        { "fieldPath": "timestamp", "order": "DESCENDING" }
-      ]
-    }
-  ]
-}
+
