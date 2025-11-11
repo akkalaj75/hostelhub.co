681c5af228f271fe16edab1687c47e57e912bcb7
Jyothin manikanta reddy <158010808+akkalaj75@users.noreply.github.com>
2025-11-11 16:09:11 -0500

    firestore.indexes.json

--- fix-bugs.js
+++ fix-bugs.js
@@ -1,1 +1,13 @@
-
+{
+  "indexes": [
+    {
+      "collectionGroup": "waiting",
+      "queryScope": "COLLECTION",
+      "fields": [
+        { "fieldPath": "hostel", "order": "ASCENDING" },
+        { "fieldPath": "interests", "arrayConfig": "CONTAINS" },
+        { "fieldPath": "timestamp", "order": "DESCENDING" }
+      ]
+    }
+  ]
+}
